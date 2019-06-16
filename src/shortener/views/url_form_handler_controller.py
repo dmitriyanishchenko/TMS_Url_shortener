fb8b40df4038a4749b3d1ae025bd846ce3dd9f37
Ditriy Anishchenko <1348010@gmail.com>
2019-06-16 17:50:36 +0300

    edit folder templates

--- src/shortener/views/url_form_handler_controller.py
+++ src/shortener/views/url_form_handler_controller.py
@@ -15,7 +15,7 @@ def url_form_handler(request):
     urls_from_db = ShortUrls.objects.all()
     if request.method == 'GET':
         context = {'form': UrlForm(), 'urls_from_db': urls_from_db, 'HOST': HOST}
-        return render(request, 'home.html', context)
+        return render(request, 'shortener/home.html', context)
     elif request.method == "POST":
         form = UrlForm(request.POST)
         if form.is_valid():
@@ -23,14 +23,14 @@ def url_form_handler(request):
             if ShortUrls.objects.filter(long_url=long_url).exists():
                 current_url = ShortUrls.objects.get(long_url=long_url)
                 context = {'short_url': f'{HOST}/{current_url.short_url}', 'HOST': HOST}
-                return render(request, 'edit_url.html', context)
+                return render(request, 'shortener/edit_url.html', context)
             else:
                 current_url = ShortUrls(long_url=long_url)
                 current_url.save()
                 current_url.short_url = base_62_encode(current_url.id)
                 current_url.save()
                 context = {'short_url': f'{HOST}/{current_url.short_url}', 'HOST': HOST}
-                return render(request, 'edit_url.html', context)
+                return render(request, 'shortener/edit_url.html', context)
         else:
             errors = form.errors
             return HttpResponse(errors)
